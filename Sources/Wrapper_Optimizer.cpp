b3f64696d3a848aabfe1bedd1962e65e96485e5e
Soumendu003 <soumendughorui.wb@gmail.com>
2020-02-23 17:51:28 +0530

    Wrapper Optimizer Update

--- Sources/Wrapper_Optimizer.cpp
+++ Sources/Wrapper_Optimizer.cpp
@@ -1,17 +1,32 @@
 #include "Wrapper_Optimizer.h"
 
-void Wrapper_Optimizer::Minimize_TSV_Phase()
+uint64_t Wrapper_Optimizer::Get_Count_TSV()
 {
-    uint64_t sc_id = rand() %_sc_array->size() ;
-
+    return _tsv_count ;
 }
 
-void Wrapper_Optimizer::Two_Phase_Optimizer()
+uint64_t Wrapper_Optimizer::Get_Test_Time()
 {
-    //Initialize_WrapperChains() ;    //Initializes the Wrapper Chain
+    return _test_time ;
+}
 
-    //Count_TSV() ;   // Counts the current TSV
-    //Count_TT() ;    // Counts current Test Time
+void Wrapper_Optimizer::Clear_Wrapper_Assignments()
+{
+    for (uint64_t i = 0; i < _sc_array->size(); i++)
+    {
+        _sc_array->at(i).wrapper_chain = 0 ;
+    }
+}
+
+void Wrapper_Optimizer::Two_Phase_Optimizer(uint8_t max_layer, uint8_t wrapper_chain_count)
+{
+    _wc_count = wrapper_chain_count ;
+    _max_layer = max_layer ;
+    
+    Initialize_WrapperChains() ;    //Initializes the Wrapper Chain
+
+    Count_TSV() ;   // Counts the current TSV
+    Count_TT() ;    // Counts current Test Time
 
     Simulated_Annelation() ;
 
@@ -40,3 +55,8 @@ void Wrapper_Optimizer::Simulated_Annelation()
     }
 }
 
+void Wrapper_Optimizer::Minimize_TSV_Phase()
+{
+    uint64_t sc_id = rand() %_sc_array->size() ;
+
+}
--- Sources/Wrapper_Optimizer.h
+++ Sources/Wrapper_Optimizer.h
@@ -8,19 +8,28 @@
 class Wrapper_Optimizer
 {
     public:
-        explicit Wrapper_Optimizer(vector<scanchain_t> *sc_array) : _sc_array(sc_array) {}
-        uint64_t Get_Count_TSV() {return 0 ;}
-        uint64_t Get_Test_Time() {return 0 ;}
-        void Two_Phase_Optimizer() ;
+        explicit Wrapper_Optimizer(vector<scanchain_t> *sc_array) :
+         _sc_array(sc_array), _tsv_count(0), _test_time(0) {}
+        uint64_t Get_Count_TSV() ;
+        uint64_t Get_Test_Time() ;
+        void Two_Phase_Optimizer(uint8_t max_layer, uint8_t wrapper_chain_count) ;
+        void Clear_Wrapper_Assignments() ;
 
     private:
         void Simulated_Annelation() ;
         void Initialize_WrapperChains() {}
         void Minimize_TSV_Phase() ;
         void Minimize_TT_Phase() {}
+        void Count_TSV() {}
+        void Count_TT() {}
+        void Initialize_WrapperChains() {}
 
     private:
         vector<scanchain_t> *_sc_array ;
+        uint64_t _tsv_count ;
+        uint64_t _test_time ;
+        uint8_t _max_layer ;
+        uint8_t _wc_count ;
 };
 
 #endif
